a2ff2bd16ed165facfc79839990588f363bf2b55
masterUNG <phrombutr@gmail.com>
2016-06-19 13:23:59 +0700

    เริ่าใหม่

--- app/src/main/java/appewtc/masterung/cartoonbook/ConfirmActivity.java
+++ app/src/main/java/appewtc/masterung/cartoonbook/ConfirmActivity.java
@@ -1,23 +1,11 @@
 package appewtc.masterung.cartoonbook;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -66,67 +54,8 @@ public class ConfirmActivity extends AppCompatActivity {
 
     private void createListView() {
 
-        //Read from SQLite
-        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
-                MODE_PRIVATE, null);
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM orderTABLE", null);
-        cursor.moveToFirst();
-
-        productIDStrings = new String[cursor.getCount()];
-        amountStrings = new String[cursor.getCount()];
-        productNameStrings = new String[cursor.getCount()];
-        productPriceStrings = new String[cursor.getCount()];
-        totalStrings = new String[cursor.getCount()];
-
-        for (int i=0;i<cursor.getCount();i++) {
-
-            productIDStrings[i] = cursor.getString(cursor.getColumnIndex("ProductID"));
-            Log.d("19JuneV2", "productID แรก ==> " + productIDStrings[i]);
-            amountStrings[i] = cursor.getString(cursor.getColumnIndex("Amount"));
-            productNameStrings[i] = myFindProduct(0, productIDStrings[i]);
-            productPriceStrings[i] = myFindProduct(1, productIDStrings[i]);
-
-            int intPrice = Integer.parseInt(productPriceStrings[i]);
-            int intAmount = Integer.parseInt(amountStrings[i]);
-            int intTotal = intPrice * intAmount;
-            totalStrings[i] = Integer.toString(intTotal);
-
-            cursor.moveToNext();
-        }   // for
-
-
-    }   // createListView
-
-    private String myFindProduct(int intIndex, String productIDString) {
-
-        String result = null;
-        String[] columnStrings = new String[]{"Name", "Price"};
-        String[] dummy = new String[]{"test", "100"};
-        Log.d("19JuneV2", "productID ==> " + productIDString);
-
-        OkHttpClient okHttpClient = new OkHttpClient();
-        RequestBody requestBody = new FormEncodingBuilder()
-                .add("isAdd", "true")
-                .add("id", productIDString)
-                .build();
-        Request.Builder builder = new Request.Builder();
-        Request request = builder.url(urlJSON).post(requestBody).build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Response response) throws IOException {
-                String strResponse = response.body().string();
-                Log.d("19JuneV2", "JSON ==> " + strResponse);
-            }
-        });
-
-        return dummy[intIndex];
     }
+
 
     public void clickAddMore(View view) {
         finish();
